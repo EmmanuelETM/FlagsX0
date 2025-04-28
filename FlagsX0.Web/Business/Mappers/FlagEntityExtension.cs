28a41ecbd8e9fb1f35b8f6e19653c3351d42cf0d
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-04-27 22:32:10 -0400

    Created Object mapper

---
+++ FlagsX0.Web/Business/Mappers/FlagEntityExtension.cs
@@ -0,0 +1,18 @@
+﻿using FlagsX0.Web.Data.Entities;
+using FlagsX0.Web.DTOs;
+
+namespace FlagX0.Web.Business.Mappers
+{
+    public static class FlagEntityExtension
+    {
+        public static FlagDTO ToDto(this FlagEntity entity)
+            => new FlagDTO()
+            {
+                IsEnabled = entity.Value,
+                Name = entity.Name,
+            };
+
+        public static List<FlagDTO> ToDTO(this List<FlagEntity> entities)
+            => entities.Select(ToDto).ToList();
+    }
+}
