68cecdbd42d625aa419b253b35fb89bdd1a454fa
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-04-28 20:06:54 -0400

    Added abstraction for user Id

--- FlagsX0.Web/Business/UseCases/Flags/GetFlagsUseCase.cs
+++ FlagsX0.Web/Business/UseCases/Flags/GetFlagsUseCase.cs
@@ -1,7 +1,7 @@
-﻿using FlagsX0.Web.Data;
+﻿using FlagsX0.Web.Business.UserInfo;
+using FlagsX0.Web.Data;
 using FlagsX0.Web.DTOs;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
 namespace FlagsX0.Web.Business.UseCases.Flags
 {
@@ -9,18 +9,23 @@ namespace FlagsX0.Web.Business.UseCases.Flags
     {
         private readonly ApplicationDbContext _aplicationDbContext;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IFlagUserDetails _userDetails;
 
-        public GetFlagsUseCase(ApplicationDbContext aplicationDbContext, IHttpContextAccessor httpContextAccessor)
+        public GetFlagsUseCase(ApplicationDbContext aplicationDbContext, IHttpContextAccessor httpContextAccessor, IFlagUserDetails flagUserDetails)
         {
             _aplicationDbContext = aplicationDbContext;
             _httpContextAccessor = httpContextAccessor;
+            _userDetails = flagUserDetails;
         }
 
         public async Task<List<FlagDTO>> Execute()
         {
-            string userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            var response = await _aplicationDbContext.Flags.Where(x => x.UserId == userId).AsNoTracking().ToListAsync();
+            var response = await _aplicationDbContext
+                .Flags
+                .Where(x => x.UserId == _userDetails.UserId)
+                .AsNoTracking()
+                .ToListAsync();
 
             return response.Select(a => new FlagDTO()
             {
---
+++ FlagsX0.Web/Business/UserInfo/FlagUserDetails.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FlagsX0.Web.Business.UserInfo
+{
+    public interface IFlagUserDetails
+    {
+        public string UserId { get; }
+    }
+
+    public class FlagUserDetails : IFlagUserDetails
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public FlagUserDetails(IHttpContextAccessor httpContextAccessor, UserManager<IdentityUser> userManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+        }
+
+        public string UserId
+            => _userManager.GetUserId(_httpContextAccessor.HttpContext!.User) ?? throw new Exception("This workflow needs authentication!");
+    }
+}
