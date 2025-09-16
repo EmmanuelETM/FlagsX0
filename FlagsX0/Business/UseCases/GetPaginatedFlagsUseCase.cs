9cd3aaa00714aac587b45fc75c6797d7ff132349
Emmanuel Torres <torresmalenaemmanuel@gmail.com>
2025-09-16 18:07:17 -0400

    Validated the input for the Page and PageSize for the Pagination.

--- FlagsX0/Business/UseCases/GetPaginatedFlagsUseCase.cs
+++ FlagsX0/Business/UseCases/GetPaginatedFlagsUseCase.cs
@@ -15,10 +15,33 @@ public class GetPaginatedFlagsUseCase(ApplicationDbContext dbContext, IFlagUserD
 
     public async Task<Result<Pagination<FlagDto>>> Execute(string? search, int page, int size)
     {
-        return await GetFromDb(search, page, size)
+        return await ValidatePage(page).Fallback(_ =>
+            {
+                page = 1;
+                return Result.Unit;
+            })
+            .Bind(_ => ValidateSize(size).Fallback(_ =>
+            {
+                size = 5;
+                return Result.Unit;
+            }))
+            .Async()
+            .Bind(x => GetFromDb(search, page, size))
             .Map(x => x.ToDto())
             .Combine(x => TotalElements(search))
             .Map(x => new Pagination<FlagDto>(x.Item1, x.Item2, size, page, search));
+    }
+
+    private Result<Unit> ValidatePage(int page)
+    {
+        return page < 1 ? Result.Failure("Page not supported") : Result.Unit;
+    }
+
+    private Result<Unit> ValidateSize(int size)
+    {
+        int[] allowedValues = [5, 10, 15];
+
+        return !allowedValues.Contains(size) ? Result.Failure("Page size not supported") : Result.Unit;
     }
 
     private async Task<Result<List<FlagEntity>>> GetFromDb(string? search, int page, int size)
